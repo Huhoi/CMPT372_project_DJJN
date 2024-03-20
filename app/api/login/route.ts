3c117cb6fbd210804d530f5b59226989a20e9146
nchow0917 <nathan17chow@gmail.com>
2024-03-19 21:04:16 -0700

    register error checking

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -3,37 +3,21 @@ import { NextResponse } from "next/server";
 import { NextApiRequest, NextApiResponse } from "next";
 import pool from '../../utils/connectDB';
 
-export const config = {
-    api: {
-        bodyParser: true,
-    },
-};
-
-// To handle a GET request to /api/login
 export async function POST(req: Request) {
 
     if (req.method === 'POST') {
         try {
             const { username, password } = await req.json();
-            // var username = 'user1';
-            // var password = 'password';
             console.log('Received username:', username);
             console.log('Received password:', password);
 
             const client = await pool.connect();
-            const result = await client.query('SELECT * FROM users WHERE username = $1', [username]);
+            const result = await client.query('SELECT * FROM users WHERE username = $1 AND password = $2', [username, password]);
 
+            // User not found or incorrect password
             if (result.rows.length === 0) {
-                // User not found
                 client.release();
-                return NextResponse.json({ error: 'Invalid username or password1' });
-            }
-
-            const user = result.rows[0];
-            if (user.password !== password) {
-                // Incorrect password
-                client.release();
-                return NextResponse.json({ error: 'Invalid username or password2' });
+                return NextResponse.json({ error: 'Invalid username or password' });
             }
 
             // Authentication successful
