08a24d6588a22ed271a73d3f1b9353bed78af1d9
Jason <jsntruong03@gmail.com>
2024-03-20 21:38:04 -0700

    Added ingredient API

--- app/pages/signedIn/inventory/page.tsx
+++ app/pages/signedIn/inventory/page.tsx
@@ -72,7 +72,7 @@ export default function InventoryPage() {
     }
 
     useEffect(() => {
-        if(sessionData){
+        if (sessionData) {
             fetchIngredients();
         }
         else
