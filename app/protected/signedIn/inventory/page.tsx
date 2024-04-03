7c23baa9f00dc0dd61ad5e550fe3c294865bc4eb
Jason Zhou <jasonzhou123@hotmail.com>
2024-04-02 19:36:18 -0700

    add styling and fix add ingredient modal logic

--- app/protected/signedIn/inventory/page.tsx
+++ app/protected/signedIn/inventory/page.tsx
@@ -6,10 +6,16 @@ import AddButton from "@/app/ui/inventory/AddButton";
 import AddModal from "@/app/ui/inventory/AddModal";
 import { Category, Ingredient } from "@/app/utils/interfaces";
 
+interface ModalState {
+    [cid: number]: boolean;
+}
+
 export default function InventoryPage() {
     const [ingredients, setIngredients] = useState<Ingredient[]>([]);
     const [categories, setCategories] = useState<Category[]>([]);
-    const [isOpen, setIsOpen] = useState(false);
+
+    // Keep track of which modal is open for each category
+    const [modalState, setModalStates] = useState<ModalState>({});
 
     const uid = useTestContext();
 
@@ -79,13 +85,19 @@ export default function InventoryPage() {
         return date.toISOString().split('T')[0];
     };
 
-    const handleModalClose = () => {
-        setIsOpen(false);
-        fetchIngredients();
+    const handleModalOpen = (cid: number) => {
+        setModalStates(prevStates => ({
+            ...prevStates,
+            [cid]: true
+        }));
     }
 
-    const handleModalOpen = () => {
-        setIsOpen(true);
+    const handleModalClose = (cid: number) => {
+        setModalStates(prevStates => ({
+            ...prevStates,
+            [cid]: false
+        }));
+        fetchIngredients();
     }
 
     return (
@@ -94,24 +106,36 @@ export default function InventoryPage() {
             <div className="flex justify-center items-center h-screen">
                 <div className="flex flex-wrap gap-4">
                     {categories.map(category => (
-                        <div key={category.cid} className="p-4 border border-gray-200 rounded">
+                        <div key={category.cid} className="py-4 px-4 w-full bg-white border-b dark:bg-gray-800 dark:border-gray-700 rounded">
                             <div className="flex justify-between items-center mb-2">
-                                <h2 className="text-lg font-semibold">{category.category_name}</h2>
+                                <h2 className="text-lg text-white">{category.category_name}</h2>
                                 <button onClick={() => handleDeleteCategory(category.cid)} className="text-red-500 ml-2">Delete Category</button>
                             </div>
-                            <ul>
-                                {ingredients
-                                    .filter(ingredient => ingredient.cid === category.cid)
-                                    .map(ingredient => (
-                                        <li key={ingredient.iid} className="mb-1">
-                                            {ingredient.ingredient_name + " "}
-                                            {ingredient.amount} {ingredient.amount_type + " "} 
-                                            {convertDate(new Date(ingredient.expiration))}
-                                            <button onClick={() => handleDeleteIngredient(ingredient.iid)} className="text-red-500 ml-2">Delete Ingredient</button>
-                                        </li>
+                            <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                                <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
+                                    <tr>
+                                        <th className="px-6 py-3">Item</th>
+                                        <th className="px-6 py-3">Amount</th>
+                                        <th className="px-6 py-3">Expiration</th>
+                                        <th className="px-6 py-3"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    {ingredients
+                                        .filter(ingredient => ingredient.cid === category.cid)
+                                        .map(ingredient => (
+                                            <tr key={ingredient.iid} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
+                                                <td className="px-6 py-4">{ingredient.ingredient_name}</td>
+                                                <td className="px-6 py-4">{ingredient.amount} {ingredient.amount_type}</td>
+                                                <td className="px-6 py-4">{convertDate(new Date(ingredient.expiration))}</td>
+                                                <td className="px-6 py-4">
+                                                    <button onClick={() => handleDeleteIngredient(ingredient.iid)} className="text-red-500 ml-2">Delete Ingredient</button>
+                                                </td>
+                                            </tr>
                                     ))}
-                            </ul>
-                            <AddModal cid={category.cid} isOpen={isOpen} onClose={handleModalClose} onOpen={handleModalOpen}/>
+                                </tbody>
+                            </table>
+                            <AddModal cid={category.cid} isOpen={modalState[category.cid] || false} onClose={() => handleModalClose(category.cid)} onOpen={() => handleModalOpen(category.cid)}/>
                         </div>
                     ))}
                 </div>
--- app/ui/inventory/AddModal.tsx
+++ app/ui/inventory/AddModal.tsx
@@ -12,7 +12,6 @@ interface AddModalProps {
 
 // modal for adding a new category using Modal component
 const AddModal: React.FC<AddModalProps> = ({ cid , isOpen , onClose, onOpen}) => {
-    // const [isOpen, setIsOpen] = useState(false);
     const [ingredientName, setIngredientName] = useState('' as string);
     const [expiration, setExpiration] = useState(null as Date | null);
     const [amount, setAmount] = useState(0 as number);
@@ -20,9 +19,6 @@ const AddModal: React.FC<AddModalProps> = ({ cid , isOpen , onClose, onOpen}) =>
 
     const openModal = () => onOpen();
     const closeModal = () => onClose();
-
-    // const openModal = () => setIsOpen(true);
-    // const closeModal = () => setIsOpen(false);
 
     const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
@@ -56,39 +52,64 @@ const AddModal: React.FC<AddModalProps> = ({ cid , isOpen , onClose, onOpen}) =>
 
     return (
         <>
-            <button className="px-4 py-2 bg-blue-500 text-white rounded" onClick={openModal}>
+            <button className="py-4 px-2 my-4 h-10 font-dm_sans tracking-tighter font-bold bg-indigo-600 hover:bg-indigo-700 text-white col-start-5 col-end-5 rounded-md flex justify-center items-center" onClick={openModal}>
                 Add Ingredient
             </button>
             <Modal modalTitle="Add Ingredient" isOpen={isOpen} onClose={closeModal}>
-                <form onSubmit={handleSubmit}>
-                    <label htmlFor="ingredient">Ingredient Name: </label>
-                    <input
-                        type="text"
-                        id="ingredient"
-                        value={ingredientName}
-                        onChange={(e) => setIngredientName(e.target.value)}
-                    /> <br />
-                    <label htmlFor="expiration">Expiration Date: </label>
-                    <input
-                        type="date"
-                        id="expiration"
-                        onChange={(e) => setExpiration(new Date(e.target.value))}
-                    /> <br />
-                    <label htmlFor="amount">Amount: </label>
-                    <input
-                        type="number"
-                        id="amount"
-                        value={amount}
-                        onChange={(e) => setAmount(e.target.valueAsNumber)}
-                    />
-                    <select name="amountType" id="amountType" value={amountType} onChange={(e) => setAmountType(e.target.value as AmountType)}>
-                        {Object.values(AmountType).map((type) => (
-                            <option key={type} value={type}>{type}</option>
-                        ))}
-                    </select>
-                    <br />
-                    <button type="submit">Add</button><br></br>
-                    <button type="button" onClick={closeModal}>Close</button>
+                <form onSubmit={handleSubmit} className="font-dm_sans h-[580px]">
+                    <div id="nameInput" className="py-2">
+                        <p className="py-2 text-2xl">Ingredient Name</p>
+                        <input
+                            type="text"
+                            id="ingredient"
+                            autoComplete="off"
+                            value={ingredientName}
+                            className="bg-slate-200 appearance-none border-2 border-slate-300 rounded w-full py-2 px-4 text-slate-600 leading-tight focus:outline-none focus:text-slate-950 focus:border-slate-400"
+                            required
+                            placeholder="Enter Ingredient Name"
+                            onChange={(e) => setIngredientName(e.target.value)}
+                        />
+                    </div>
+
+                    <div id="expirationInput" className="py-2">
+                        <p className="py-2 text-2xl">Expiration Date</p>
+                        <input
+                            type="date"
+                            id="expiration"
+                            required
+                            onChange={(e) => setExpiration(new Date(e.target.value))}
+                            className="resize-none bg-slate-200 appearance-none border-2 border-slate-300 rounded w-1/4 py-2 px-4 text-slate-600 leading-tight focus:outline-none focus:text-slate-950 focus:border-slate-400"
+                        /> 
+                    </div>
+
+                    
+                    <div id="amountInput" className="py-2">
+                        <p className="py-2 text-2xl">Amount</p>
+                        <input
+                            type="number"
+                            id="amount"
+                            required
+                            autoComplete="off"
+                            className="resize-none bg-slate-200 appearance-none border-2 border-slate-300 rounded py-2 px-4 text-slate-600 leading-tight focus:outline-none focus:text-slate-950 focus:border-slate-400"
+                            value={amount}
+                            onChange={(e) => setAmount(e.target.valueAsNumber)}
+                            />
+                        <select 
+                            name="amountType" 
+                            id="amountType" 
+                            value={amountType} 
+                            onChange={(e) => setAmountType(e.target.value as AmountType)}
+                            className="resize-none bg-slate-200 appearance-none border-2 border-slate-300 rounded py-2 px-4 text-slate-600 leading-tight focus:outline-none focus:text-slate-950 focus:border-slate-400">
+                                {Object.values(AmountType).map((type) => (
+                                    <option key={type} value={type}>{type}</option>
+                                ))}
+                        </select>
+                    </div>
+
+                    <div id="buttonContainer" className="absolute px-4 bottom-0 left-0 h-1/8 w-full bg-gradient-to-r from-blue-100 to-indigo-100 grid grid-cols-5 grid-rows-1 gap-2 justify-center items-center">
+                        <button type="button" onClick={closeModal} className="py-4 my-4 h-10 font-dm_sans tracking-tighter font-bold col-start-1 col-end-1 hover:bg-slate-900/10 text-slate-500 hover:text-slate-950 rounded-md flex justify-center items-center">Close</button>
+                        <button type="submit" className="py-4 my-4 h-10 font-dm_sans tracking-tighter font-bold bg-indigo-600 hover:bg-indigo-700 text-white col-start-5 col-end-5 rounded-md flex justify-center items-center">Add</button>
+                    </div>
                 </form>
             </Modal>
         </>
