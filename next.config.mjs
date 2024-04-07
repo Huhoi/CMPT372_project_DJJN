62f452c53449b0b9f0fea7dbb9d9127d143469ef
Jason <jsntruong03@gmail.com>
2024-04-07 00:26:44 -0700

    Possibly added fix to image from external api not showing

--- next.config.mjs
+++ next.config.mjs
@@ -8,8 +8,23 @@ const nextConfig = {
         ignoreBuildErrors: true,
     },
     images: {
-        domains: ['img.spoonacular.com'], // Add the domain of the image source
+        domains: ['img.spoonacular.com', 'https://img.spoonacular.com/'], // Add the domain of the image source
     },
+
+    async headers() {
+        return [
+            {
+                // matching all API routes
+                source: "/api/:path*",
+                headers: [
+                    { key: "Access-Control-Allow-Credentials", value: "true" },
+                    { key: "Access-Control-Allow-Origin", value: "*" }, // replace this your actual origin
+                    { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT" },
+                    { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+                ]
+            }
+        ]
+    }
 };
 
 export default nextConfig;
